1139535ac807bbbdaab2d3203686e7c06f160cfc
thangtm2 <thangtm2@vng.com.vn>
2019-10-12 06:08:43 +0700

    Updates

---
+++ precache-manifest.7cf372da8bf2833e33a6b06a4752cf75.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0029e73b73e252f83ca21495a0d52ce2",
+    "url": "/webexcercises/index.html"
+  },
+  {
+    "revision": "aa88943ed89bc44908bb",
+    "url": "/webexcercises/static/css/main.7fd559b0.chunk.css"
+  },
+  {
+    "revision": "7a75e4605075e4185c56",
+    "url": "/webexcercises/static/js/2.de8066cb.chunk.js"
+  },
+  {
+    "revision": "aa88943ed89bc44908bb",
+    "url": "/webexcercises/static/js/main.d570d952.chunk.js"
+  },
+  {
+    "revision": "6137fe7a499ca81d0b52",
+    "url": "/webexcercises/static/js/runtime-main.3168af50.js"
+  }
+]);
